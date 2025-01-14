b6fb12263d80ee104e29028772eeed5e0177590e
Narcisseuuh <augustin.perrin03@gmail.com>
2025-01-14 15:15:59 +0100

    challenge 3 finished

--- hello.py
+++ hello.py
@@ -1,15 +1,33 @@
-import datetime
-from flask import Flask, render_template
-import socket
 import os
+import socket
+import datetime
+from flask import Flask, request, render_template
+import pymongo
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hello_world():
-    server_hostname = socket.gethostname()
-    date = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=1)))
-    return render_template('index.html', dt=date, serv_host=server_hostname)
+    try:
+        server_hostname = socket.gethostname()
+        date = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=1)))
+        
+        client_ip = request.remote_addr
+        
+        MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://narcisse:adminadmin@localhost:27017/')
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client["database_de_narcisse"]
+        collection = db["collection_de_narcisse"]
+        
+        # Insert the new record
+        collection.insert_one({"client_ip": client_ip, "date": date})
+        
+        # Retrieve the last 10 records
+        records = list(collection.find().sort("_id", -1).limit(10))
+        
+        return render_template('index.html', dt=date, serv_host=server_hostname, records=records)
+    except pymongo.errors.PyMongoError as e:
+        return f"An error occurred: {e}", 500
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5066))
